b0d73eaced84565adba7ebf380985a7b8931ee86
manuguse <emanuguse@gmail.com>
2024-05-15 22:47:39 -0300

    tarefa 5

--- tarefa-5/exercicio-1.asm
+++ tarefa-5/exercicio-1.asm
@@ -1,41 +1,33 @@
-# Escreva um programa em linguagem Assembly do MIPS para ser executado no simulador MARS.
-# Este programa deverá calcular o fatorial de um número e deve atender às seguintes premissas:
-# • Receba via teclado o valor do número a ser calculado o fatorial;
-# • Efetue o cálculo do fatorial sem o uso de procedimentos; e
-# • Mostre o resultado na tela do computador.
-
 .data
 
     mensagem_input: .asciiz "Digite um numero: "
-    mensagem_output: .asciiz "O fatorial do numero é: "
+    mensagem_output: .asciiz "O fatorial do numero Ã©: "
 
 .text
 
 main:
 
-    # pede o valor do número a ser calculado o fatorial
-    # li $v0, 4
-    # la $a0, mensagem_input
-    # syscall
+    # pede o valor do nÃºmero a ser calculado o fatorial
+    li $v0, 4
+    la $a0, mensagem_input
+    syscall
 
-    # recebe o valor do número a ser calculado o fatorial e armazena em $t0
-    # li $v0, 5
-    # syscall
-    # move $t0, $v0
-    
-    li $t0, 12
+    # recebe o valor do nÃºmero a ser calculado o fatorial e armazena em $t0
+    li $v0, 5
+    syscall
+    move $t0, $v0
 
     # inicializa o registrador $t1 com o valor 1
-    li $t1, 0  # isso será o contador do loop
+    li $t1, 0  # isso serÃ¡ o contador do loop
 
     # inicializa o registrador $t2 com o valor 1
-    li $t2, 1  # isso será o fatorial do número
+    li $t2, 1  # isso serÃ¡ o fatorial do nÃºmero
 
-    # calcula o fatorial do número
+    # calcula o fatorial do nÃºmero
     loop:
         addi $t1, $t1, 1    # i += 1
         mul $t2, $t2, $t1   # fatorial *= i
-        bne $t1, $t0, loop  # se i != n, então repete o loop
+        bne $t1, $t0, loop  # se i != n, entÃ£o repete o loop
 
     # mostra a mensagem do resultado
     li $v0, 4
--- tarefa-5/exercicio-2.asm
+++ tarefa-5/exercicio-2.asm
@@ -1,46 +1,21 @@
-# Escreva um programa em linguagem Assembly do MIPS para ser executado no simulador MARS.
-# Este programa dever? calcular o fatorial de um n?mero e deve atender ?s seguintes premissas:
-# ? Receba via teclado o valor do n?mero a ser calculado o fatorial;
-# ? Chame uma fun??o fatorial( ) - procedimento ? para calcular, de modo recursivo, o fatorial
-# do n?mero; e
-# ? Mostre o resultado na tela do computador.
-
-# int fatorial(int n) {
-#     if (n == 0) {
-#         return 1;
-#     } else {
-#         return n * fatorial(n - 1);
-#     }
-# }
-
-# int main() {
-#     int n;
-#     cout << "Digite um n?mero: ";
-#     cin >> n;
-#     cout << "O fatorial de " << n << " ? " << fatorial(n) << endl;
-#     return 0;
-# }
-
 .data
 
     mensagem_input: .asciiz "Digite um numero: "
-    mensagem_output: .asciiz "O fatorial do numero �: "
+    mensagem_output: .asciiz "O fatorial do numero é: "
 
 .text
 
 main:
 
-    # pede o valor do n?mero a ser calculado o fatorial
-    # li $v0, 4
-    # la $a0, mensagem_input
-    # syscall
+    # pede o valor do número a ser calculado o fatorial
+    li $v0, 4
+    la $a0, mensagem_input
+    syscall
 
-    # recebe o valor do n?mero a ser calculado o fatorial e armazena em $a0, que ? o primeiro argumento
-    # li $v0, 5
-    # syscall
-    # move $a0, $v0
-
-    li $a0, 12
+    # recebe o valor do número a ser calculado o fatorial e armazena em $a0, que é o primeiro argumento
+    li $v0, 5
+    syscall
+    move $a0, $v0
 
     # chama a fun??o fatorial
     jal fatorial
@@ -62,23 +37,23 @@ main:
 
 fatorial:
 
-    addi $sp, $sp, -8  # decrementa o ponteiro de pilha
-    sw $ra, 0($sp)  # salva o endere?o de retorno
-    sw $s0, 4($sp)  # salva o argumento n
-    addi $v0, $zero, 1  # retorna 1
-    bne $a0, $zero, fatorial_else  # se n != 0, ent?o vai para fatorial_else
+    addi $sp, $sp, -8              # decrementa o ponteiro de pilha
+    sw $ra, 0($sp)                 # salva o endereço de retorno
+    sw $s0, 4($sp)                 # salva o argumento n
+    addi $v0, $zero, 1             # retorna 1
+    bne $a0, $zero, fatorial_else  # se n != 0, então vai para fatorial_else
     j fatorial_end
 
 fatorial_else:
 
-    move $s0, $a0  # salva o argumento n em $s0
+    move $s0, $a0      # salva o argumento n em $s0
     addi $a0, $a0, -1  # decrementa n
-    jal fatorial  # chama a fun??o fatorial
+    jal fatorial       # chama a função fatorial
     mul $v0, $s0, $v0  # retorna n * fatorial(n - 1)        
 
 fatorial_end:
     
-    lw $ra, 0($sp)  # restaura o endere?o de retorno
-    lw $s0, 4($sp)  # restaura o argumento n
+    lw $ra, 0($sp)    # restaura o endere?o de retorno
+    lw $s0, 4($sp)    # restaura o argumento n
     addi $sp, $sp, 8  # incrementa o ponteiro de pilha
-    jr $ra  # retorna para o endere?o de retorno
+    jr $ra            # retorna para o endereço de retorno
